fbc0d459318b3e5e892070e2e5a9e85eff02425a
zacherycardoza <85895258+zacherycardoza@users.noreply.github.com>
2025-09-05 09:42:05 -0400

    topics articles usertopics migrations

---
+++ database/migrations/2025_09_05_014908_create_articles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->longText('content');
+            $table->string('url')->unique();
+            $table->string('source');
+            $table->timestamp('published_at')->nullable();
+            $table->longText('summary')->nullable();
+            $table->foreignId('topic_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
