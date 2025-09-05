d4c30e6bb49953d7efa3dfabfffd011a30d03bd7
zacherycardoza <85895258+zacherycardoza@users.noreply.github.com>
2025-09-05 11:34:59 -0400

    login redirects to feed

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -21,7 +21,7 @@ class LoginController extends Controller
 
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
-            return redirect()->intended('/');
+            return redirect()->intended('/feed');
         }
 
         return back()->withErrors([
