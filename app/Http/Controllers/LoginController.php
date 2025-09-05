c9b7691b39c60cb4941bbe3a2784fb2de720c539
zacherycardoza <85895258+zacherycardoza@users.noreply.github.com>
2025-09-04 21:38:08 -0400

    login and register methods

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -11,8 +12,20 @@ class LoginController extends Controller
         return view('auth.login');
     }
 
-    function login()
+    function login(Request $request)
     {
-        return null;
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended('/');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ])->onlyInput('email');
     }
 }
--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\User;
 
 class RegisterController extends Controller
 {
@@ -11,8 +15,23 @@ class RegisterController extends Controller
         return view('auth.register');
     }
 
-    function register()
+    function register(Request $request)
     {
-        return null;
+        // validate
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+        // create user
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
+        ]);
+        // login user
+        Auth::login($user);
+        // redirect
+        return redirect()->route('landing');
     }
 }
