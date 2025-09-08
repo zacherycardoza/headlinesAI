b8f1948a84c2826f2d0b5feb2939140b9b5843d9
zacherycardoza <85895258+zacherycardoza@users.noreply.github.com>
2025-09-08 07:55:42 -0400

    routes for topics

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\FeedController;
+use App\Http\Controllers\TopicController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -27,3 +28,11 @@ Route::post('/logout', function () {
 Route::get('/feed', [FeedController::class, 'index'])
     ->middleware('auth')
     ->name('feed');
+
+Route::get('/topics', [TopicController::class, 'edit'])
+    ->middleware('auth')
+    ->name('topics');
+
+Route::put('/topics', [TopicController::class, 'update'])
+    ->middleware('auth')
+    ->name('topics.update');
